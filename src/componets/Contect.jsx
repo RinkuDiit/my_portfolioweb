6eeff4bde98406d7c80573f86631870315613734
Rinku <rr1341570@gmail.com>
2025-02-03 18:58:33 +0530

    changess in contact

--- src/componets/Contect.jsx
+++ src/componets/Contect.jsx
@@ -4,8 +4,10 @@ function Contect() {
     return (
         <div>
             <div className="contact_section1">
-                <h1 style={{ textAlign: 'center' }}>Contact <span style={{ color: '#8750f7' }}>Me</span></h1>
+                <h1 style={{ textAlign: 'center',width:'100%' }}>Contact <span style={{ color: '#8750f7' }}>Me</span></h1>
+                
                 <div className="contact_form">
+                    <h1 className='i_am_h1_color'>Let’s work <br /> together!</h1>
                     <form action="" method="post">
                         <input type="text" placeholder='Name' />
                         <input type='text' placeholder='Enter Your E-mail' /><br />
@@ -15,7 +17,14 @@ function Contect() {
                         <button type='submite'>Send message</button>
                     </form>
                 </div>
+                <div className="contact_info">
+                    
+                        <div className='flexForcontact'><button className='my_buttons my_linkdin'><i class="fa fa-phone" aria-hidden="true"></i></button><p>Phone <br /><a href="tel:+919983552078"> +91 9983552078</a></p></div>
+                        <div className='flexForcontact'><button className='my_buttons my_linkdin'><i class="fa fa-envelope-o" aria-hidden="true"></i></button> <p>E-mail <br /><a href="mailto:rr1341570@gmail.com"> rr1341570@gmail.com</a></p></div>
+                        <div className='flexForcontact'><button className='my_buttons my_linkdin'><i class="fa fa-location-arrow" aria-hidden="true"></i></button><p>Address<br /><a href="https://maps.app.goo.gl/JtNAqmH9sX7CGS6B6"> Govindgarh Alwar (Raj)</a> </p></div>
 
+                    
+                </div>
             </div>
         </div>
     )
